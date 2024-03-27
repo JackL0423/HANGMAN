e02470b901513c3592c13e3bf0d3de0ff890c7c0
Jack Light <jack.light2023@gmail.com>
2024-03-27 02:34:30 -0400

    Minor fixes for AVL tree.

--- headers/avl_tree.h
+++ headers/avl_tree.h
@@ -8,30 +8,16 @@
 
 typedef void* TREE;
 
-typedef struct node Node;
-
-struct node {
-    Node* left;
-    Node* right;
-    Node* parent;
-    MY_STRING key;
-    GENERIC_VECTOR data;
-};
-
-typedef struct avl_tree {
-    Node* root;
-} AVL_tree;
-
 TREE avl_tree_init_default(void);
 
 void avl_tree_destroy(TREE* phMy_tree);
-
-void avl_tree_destroy_node(Node* pNode);
 
 Status avl_tree_push(TREE hMy_tree, MY_STRING key, MY_STRING item);
 
 GENERIC_VECTOR avl_tree_get_largest_family(TREE hMy_tree, int print_val);
 
 GENERIC_VECTOR find_max_family(Node* root, int print_val);
+
+int avl_find_tree_magnitude(TREE hTree); 
 
 #endif//AVL_TREE_H
--- src/avl_tree.c
+++ src/avl_tree.c
@@ -5,98 +5,84 @@
 #include "./headers/generic.h"
 #include "./headers/avl_tree.h"
 
+typedef struct node Node;
+
+struct node {
+    Node* left;
+    Node* right;
+    MY_STRING key;
+    GENERIC_VECTOR data;
+};
+
+typedef struct node AVL_tree;
+
+//  Fix init funciton.
 TREE avl_tree_init_default(void) {
-    AVL_tree* pMy_tree = (AVL_tree*)malloc(sizeof(AVL_tree));
-    if (pMy_tree == NULL) {
+    AVL_tree* pRoot = (AVL_tree*)malloc(sizeof(AVL_tree));
+    if (pRoot == NULL) {
         return NULL;
     }
+    pRoot->left = NULL;
+    pRoot->right = NULL;
 
-    pMy_tree->root = NULL;
-
-    return pMy_tree;
+    return (TREE)pRoot;
 }
 
-void avl_tree_destroy(TREE* phMy_tree) {
-    AVL_tree* pMy_tree = (AVL_tree*)*phMy_tree;
-
-    if (pMy_tree->root != NULL) {
-        if ((pMy_tree->root)->left != NULL) {
-            avl_tree_destroy_node((pMy_tree->root)->left);
-        }
-
-        if ((pMy_tree->root)->right != NULL) {
-            avl_tree_destroy_node((pMy_tree->root)->right);
-        }
-
-        my_string_destroy(&((pMy_tree->root)->key));
-        generic_vector_destroy(&((pMy_tree->root)->data));
-        free(pMy_tree->root);
+void avl_tree_destroy(TREE* phRoot) {
+    AVL_tree* pRoot = (AVL_tree*)*phRoot;
+    if (pRoot != NULL) {
+        my_string_destroy(&(pRoot->key));
+        generic_vector_destroy(&(pRoot->data));
+        avl_tree_destroy(pRoot->left);
+        avl_tree_destroy(pRoot->right);
+        free(pRoot);
     }
-
-    free(pMy_tree);
-    phMy_tree = NULL;
+    phRoot = NULL;
 }
 
-void avl_tree_destroy_node(Node* pNode) {
-    if (pNode == NULL) {
-        return;
-    }
-
-    if (pNode->key != NULL) {
-        my_string_destroy(&(pNode->key));
-    }
-
-    if (pNode->data != NULL) {
-        generic_vector_destroy(&(pNode->data));
-    }
-
-    avl_tree_destroy_node(pNode->left);
-    avl_tree_destroy_node(pNode->right);
-
-    free(pNode);
-}
-
-Status avl_tree_push(TREE hMy_tree, MY_STRING key, MY_STRING item) {
-    AVL_tree* pMy_tree = (AVL_tree*)hMy_tree;
+//  Fix the push function and correct to use the right values.
+Status avl_tree_push(TREE hTree, MY_STRING key, MY_STRING item) {
+    AVL_tree* pRoot = (AVL_tree*)hTree;
     int flag;
 
-    if (pMy_tree->root == NULL) {
-        pMy_tree->root = (Node*)malloc(sizeof(Node));
-        if(pMy_tree->root == NULL) {
+    if (pRoot == NULL) {
+        pRoot = (Node*)malloc(sizeof(Node));
+        if (pRoot == NULL) {
             return FAILURE;
         }
-        (pMy_tree->root)->left = NULL;
-        (pMy_tree->root)->right = NULL;
-        (pMy_tree->root)->key = my_string_init_c_string(my_string_c_str(key));
+        pRoot->left = NULL;
+        pRoot->right = NULL;
+        pRoot->key = my_string_init_c_string(my_string_c_str(key));
 
-        (pMy_tree->root)->data = generic_vector_init_default((ITEM)my_string_assignment, my_string_destroy);
-        generic_vector_push_back((pMy_tree->root)->data, item);
+        pRoot->data = generic_vector_init_default((ITEM)my_string_assignment, my_string_destroy);
+        generic_vector_push_back(pRoot->data, item);
+    }
+
+    //  Need to come up with recursive solution.
+    //  Need to also add way for tree to reorginize itself if magnitute >2
+   flag = my_string_compare(pRoot->key, key);
+    if (flag == 1) {
+        avl_tree_push(&(pRoot->right), key, item);
+    }
+    else if (flag == 0) {
+        generic_vector_push_back(pRoot->data, item);
+    }
+    else if (flag == -1) {
+        avl_tree_push(&(pRoot->left), key, item);
     }
     else {
-        flag = my_string_compare((pMy_tree->root)->key, key);
-        if (flag == 1) {
-            avl_tree_push(&((pMy_tree->root)->right), key, item);
-        }
-        else if (flag == 0) {
-            generic_vector_push_back((pMy_tree->root)->data, item);
-        }
-        else if (flag == -1) {
-            avl_tree_push(&((pMy_tree->root)->left), key, item);
-        }
-        else {
-            return FAILURE;
-        }
+        return FAILURE;
     }
     return SUCCESS;
 }
 
-GENERIC_VECTOR avl_tree_get_largest_family(TREE hMy_tree, int print_val) {
-    AVL_tree* pMy_tree = (AVL_tree*)hMy_tree;
+GENERIC_VECTOR avl_tree_get_largest_family(TREE hTree, int print_val) {
+    AVL_tree* pRoot = (AVL_tree*)hTree;
     GENERIC_VECTOR return_vector = NULL;
     GENERIC_VECTOR temp;
     int indx;
 
-    temp = find_max_family(pMy_tree->root, print_val);
+    temp = find_max_family(pRoot, print_val);
     return_vector = generic_vector_init_default((ITEM)my_string_assignment, my_string_destroy);
 
     for (indx=0; indx<generic_vector_get_size(temp); indx++) {
@@ -133,4 +119,15 @@ GENERIC_VECTOR find_max_family(Node* root, int print_val) {
 
         return temp;
     }
+}
+
+
+//  Make function for balancing factor. 
+int avl_find_tree_magnitude(TREE hTree) {
+    AVL_tree* pRoot = (AVL_tree*)hTree;
+    if (pRoot != NULL) {
+        return 1 + ((avl_find_tree_magnitude(pRoot->left) > avl_find_tree_magnitude(pRoot->right)) ? avl_find_tree_magnitude(pRoot->left):avl_find_tree_magnitude(pRoot->right));
+    }
+
+    return 0;
 }
