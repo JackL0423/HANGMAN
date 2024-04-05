6f599b6adcf58614a604200327270628fe3c00d9
Jack Light <jack.light2023@gmail.com>
2024-04-05 17:51:57 -0400

    Updated string insertion.

--- src/my_string.c
+++ src/my_string.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./headers/my_string.h"
+#include "headers/my_string.h"
 
 #define GROWTH_RATE (2)
 
@@ -102,7 +102,7 @@ Status my_string_extraction(MY_STRING hMy_string, FILE* fp) {
     while (fscanf(fp, "%s", word) != EOF) {
         
         if ((int)strlen(word) >= my_string_get_capacity(hMy_string)) {
-            temp = malloc(sizeof(char) * pString->capacity * GROWTH_RATE);
+            temp = (char*)malloc(sizeof(char) * pString->capacity * GROWTH_RATE);
             if (temp == NULL) {
                 return FAILURE;
             }
@@ -130,15 +130,13 @@ Status my_string_extraction(MY_STRING hMy_string, FILE* fp) {
 
 Status my_string_insertion(MY_STRING hMy_string, FILE* fp) {
     my_string* pString = (my_string*)hMy_string;
-    int indx;
+    char* temp = my_string_c_str(hMy_string);
 
     if (pString->data == NULL) {
         return FAILURE;
     }
     else {
-        for (indx=0; indx < my_string_get_size(hMy_string); indx++) {
-            fputs(&(pString->data[indx]), fp);   
-        }
+        fprintf(fp, "%s", temp);
     }
     
     return SUCCESS;
@@ -149,20 +147,20 @@ Status my_string_push_back(MY_STRING hMy_string, char item) {
     my_string* pString = (my_string*)hMy_string;
     char* temp;
     int i;
-
-    if (my_string_empty(hMy_string)) {
+    if (hMy_string == NULL) {
         return FAILURE;
     }
 
-    if (pString->size++ >= pString->capacity) {     //  Check if string needs resize.
-        temp = malloc(sizeof(char) * pString->capacity * GROWTH_RATE);
+    if (my_string_get_size(hMy_string)+1 >= my_string_get_capacity(hMy_string)) {     //  Check if string needs resize.
+        temp = (char*)malloc(sizeof(char) * pString->capacity * GROWTH_RATE);
         if (temp == NULL) {
             return FAILURE;
         }
 
-        for (i=0; i < pString->size; i++) {
+        for (i=0; i < my_string_get_size(hMy_string); i++) {
             temp[i] = pString->data[i];
         }
+
         free(pString->data);
         pString->data = temp;
         pString->capacity *= GROWTH_RATE;
@@ -174,11 +172,8 @@ Status my_string_push_back(MY_STRING hMy_string, char item) {
 
 Status my_string_pop_back(MY_STRING hMy_string) {
     my_string* pString = (my_string*)hMy_string;
-    if (my_string_empty(hMy_string)) {
-        return FAILURE;
-    }
-
-    if (pString->size >= 0) {
+    
+    if (pString->size > 0) {
         pString->size--;
         return SUCCESS;
     }
@@ -306,14 +301,11 @@ void my_string_swap(MY_STRING hLeft, MY_STRING hRight) {
 
 Status get_word_key_value(MY_STRING current_word_family, MY_STRING new_key, MY_STRING word, char guess) {
     int indx;
-    while (!my_string_empty(new_key)) {
-        printf("Before pop...\n");
+    while (my_string_empty(new_key) == FALSE) {
         my_string_pop_back(new_key);
-        printf("After pop...\n");
     }
 
     for (indx=0; indx<my_string_get_size(word); indx++) {
-        printf("for loop in word_key.\n");
         if (!my_string_push_back(new_key, *my_string_at(word, indx) == guess ? guess : *my_string_at(current_word_family, indx))) {
             return FAILURE;
         }
